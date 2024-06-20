39d0b663bb6f91940c619c908542e6f2814682e2
Tanim Omor <tanim.pro@gmail.com>
2024-06-20 14:33:05 +0600

    Modal complete

---
+++ index.js
@@ -0,0 +1,35 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+console.log('openModalButtons', openModalButtons)
+
+openModalButtons.forEach((button) => {
+    console.log('xyz')
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+
+closeModalButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    })
+})
+
+function openModal(modal) {
+    if(modal != null){
+        modal.classList.add('active');
+        overlay.classList.add('active');
+    }
+}
+
+function closeModal(modal) {
+    if(modal != null){
+        modal.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+}
+
